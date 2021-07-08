7b771b7494635465653a98e2e57b5f51ec91f7d9
flavia <flavinha13.blessed@gmail.com>
2021-07-08 16:18:14 -0300

    exercicios

---
+++ exercicio02.java
@@ -0,0 +1,26 @@
+package exerciciosjp;
+
+import java.util.Scanner;
+
+public class exercicio02 {
+	
+		public static void main(String[] args) {
+			int idade, anos, anosresto, meses, mesesresto;
+			Scanner scan = new Scanner(System.in);
+
+			System.out.println("Insira sua idade em dias totais: ");
+			idade = scan.nextInt();
+
+			anos = idade/365;
+			System.out.print("Você tem "+ anos + " anos");
+
+			anosresto = idade%365;
+			meses = anosresto/ 28;
+			System.out.print(", "+ meses + " meses");
+
+			mesesresto = anosresto%28;
+			System.out.print(" e "+ mesesresto +" dias");
+		
+	}
+}
+
